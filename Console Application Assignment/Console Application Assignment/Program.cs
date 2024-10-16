5ab04f5131d3e52baa0b6079e6b7242583c95d5f
Darkest-Storm <KevinSurmik@gmail.com>
2024-10-16 04:18:19 -0700

    Console app and Math Comparison Op Assignments

---
+++ Console Application Assignment/Console Application Assignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Application_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt user for input
+            Console.WriteLine("Please enter a number:");
+            string userInput = Console.ReadLine();
+           
+            // Convert user input to double
+            double number = Convert.ToDouble(userInput);
+            
+            // Multiply by 50 and print result
+            double result1 = number * 50;
+            Console.WriteLine(number + " multiplied by 50 is: " + result1);
+           
+            // Add 25 and print result
+            double result2 = number + 25;
+            Console.WriteLine(number + " plus 25 is: " + result2);
+            
+            // Divide by 12.5 and print result
+            double result3 = number / 12.5;
+            Console.WriteLine(number + " divided by 12.5 is: " + result3);
+            
+            // Check if greater than 50 and print boolean result
+            bool result4 = number > 50;
+            Console.WriteLine("Is " + number + " greater than 50? " + result4);
+            
+            // Calculate remainder when divided by 7 and print result
+            double result5 = number % 7;
+            Console.WriteLine("The remainder when " + number + " is divided by 7 is: " + result5);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Math and Comparison Operators Assignment/Math and Comparison Operators Assignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_and_Comparison_Operators_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // two line intro to the program
+            Console.WriteLine("Anonymous Income Comparison Program \nPerson 1");
+
+
+            // grab person 1 hourly rate and weekly hours from the user
+            Console.WriteLine("What is the hourly rate?");
+            string hourRate1 = Console.ReadLine();
+
+            Console.WriteLine("How many hours worked per week?");
+            string hours1 = Console.ReadLine();
+
+            // grab person 2 hourly rate and weekly hours from the user
+            Console.WriteLine("What is the hourly rate>");
+            string hourRate2 = Console.ReadLine();
+
+            Console.WriteLine("How many hours worked per week?");
+            string hours2 = Console.ReadLine();
+
+            // calculate the salery and do type conversions
+            decimal salary1 = decimal.Parse(hourRate1) * Convert.ToInt32(hours1) * 52;
+
+            decimal salary2 = decimal.Parse(hourRate2) * Convert.ToInt32(hours2) * 52;
+
+            // show both salaries and then say if person 1 makes more or less in a true or false manor per reqs
+            Console.WriteLine("Person 1 has a salary of: " + salary1);
+
+            Console.WriteLine("Person 2 has a salary of: " + salary2);
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool isMore = salary1 > salary2;
+            Console.WriteLine(isMore);
+
+
+            Console.ReadLine();
+        }
+    }
+}
