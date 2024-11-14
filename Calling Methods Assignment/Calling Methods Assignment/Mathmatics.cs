41df127d2e202e158b0ae5cefcf907adf4f26d11
Darkest-Storm <KevinSurmik@gmail.com>
2024-11-14 06:36:37 -0800

    Calling Methods Assignment
    Create and call 3 methods

---
+++ Calling Methods Assignment/Calling Methods Assignment/Mathmatics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    public class Mathmatics
+    {
+        // create three methods, each of which will take one integer parameter in and return an integer.
+        // The methods should do some math operation on the received parameter.
+        public static int TimesTen(int ten)
+        {
+            int timesTen = ten * 10;
+            return timesTen;
+        }
+        public static int TimesHundred(int hundred)
+        {
+            int timesHundred = hundred * 100;
+            return timesHundred;
+        }
+        public static int TimesThousand(int thousand)
+        {
+            int timesThousand = thousand * 1000;
+            return timesThousand;
+        }
+    }
+     
+}
---
+++ Calling Methods Assignment/Calling Methods Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                //ask the user what number they want to do the math operations on
+                Console.WriteLine("What number would you like to do math on?");
+                int userMath = Convert.ToInt32(Console.ReadLine());
+
+                //Call each method in turn, passing the user input
+                Console.WriteLine(userMath + " Times 10 = " + Mathmatics.TimesTen(userMath));
+                Console.WriteLine(userMath + " Times 100 = " + Mathmatics.TimesHundred(userMath));
+                Console.WriteLine(userMath + " Times 1000 = " + Mathmatics.TimesThousand(userMath));
+
+                Console.ReadLine();
+            }
+            catch (FormatException)
+            {
+                    Console.WriteLine("Next time please only enter a whole number");
+                    Console.ReadLine();
+            }
+        }
+    }
+}
