6928b213f903283c05a3d0dd07cb4388299845c3
Darkest-Storm <KevinSurmik@gmail.com>
2024-11-15 08:20:25 -0800

    Method Class Assignment Submission

---
+++ Method Class Assignment/Method Class Assignment/MathOps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    // Create the MathOps class
+    class MathOps
+    {
+        // create a void method that takes two integers
+        
+        public void DoMathOps(int num1, int num2)
+        {
+            // do math on the first input while just displaying the second
+            int answer = num1 + 66;
+            Console.WriteLine("The result of adding 66 to " + num1 + " is: " + answer + "\nThe second number was: " + num2);
+
+        }
+    }
+}
---
+++ Method Class Assignment/Method Class Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOps class
+            MathOps mathOps = new MathOps();
+
+            // Call the method, passing two numbers
+            mathOps.DoMathOps(8, 98);
+
+            // Call the method, specifying parameters by name
+            mathOps.DoMathOps(num1: 5, num2: 10);
+
+            Console.ReadLine();
+        }
+    }
+}
