d1c5e03a2a88a0e93e42020046aa6ddd34d76511
Darkest-Storm <KevinSurmik@gmail.com>
2024-11-15 07:34:20 -0800

    Main Method Assignment(Method Overloading)
    Learned about and used Method Overloading

---
+++ MainMethodAssignment/MainMethodAssignment/MathMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class MathMethods
+    {
+        //Create a math method that returns an integer
+        public int Math (int number)
+        {
+            return number + 66;
+        }
+
+        // Create 2nd method same name, takes decimal and returns an int
+        public int Math (decimal number)
+        {
+            // convert input to int to perform math
+            int intNumber = Convert.ToInt32(number);
+            return intNumber - 66;
+        }
+
+        // Create 3rd math method, takes a string and returns an int if possible
+        public int Math (string number)
+        {
+            // convert the input string to an int to perform math
+            int intNumber = Convert.ToInt32(number);
+            return intNumber * 66;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate MathMethods class
+            MathMethods math1 = new MathMethods();
+
+            // Call first method with an interger
+            Console.WriteLine(math1.Math(7));
+
+            // Call second method with a decimal
+            Console.WriteLine(math1.Math(107.6m));
+
+            // Call third method with a string
+            Console.WriteLine(math1.Math("6"));
+
+            Console.ReadLine();
+        }
+        
+    }
+}
