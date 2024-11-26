2165cf35b6313d4fbc94437607b14d6d69548d3a
Darkest-Storm <KevinSurmik@gmail.com>
2024-11-26 09:50:44 -0800

    Datetime Assignment
    learned about and practiced with DateTime

---
+++ Datetime Assignment/Datetime Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print current date and time 
+            Console.WriteLine("\nThe date and time at this moment is: " + DateTime.Now);
+
+            // ask user for a number
+            Console.WriteLine("\nPlease provde a number to see what time it will be in that many hours: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            // print the time plus num in hours added to time
+            Console.WriteLine("In " + num + " hours it will be : " + DateTime.Now.AddHours(num));
+
+            Console.ReadLine();
+        }
+    }
+}
