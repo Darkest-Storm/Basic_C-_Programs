04e7b70c77d164941a4e556704dd2e4f805d22af
Darkest-Storm <KevinSurmik@gmail.com>
2024-11-26 09:11:25 -0800

    Input Assignment
    Learned about outputting and inputting to and from .txt

---
+++ Input Assignment/Input Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask user for a number
+            Console.WriteLine("Please enter a number");
+
+            // log the number to a text file
+            string num = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Kevin\Desktop\AOL Folder\C# and .NET Framework\Basic_C#_Programs\Input Assignment\log.txt", num);
+
+            Console.WriteLine("You have logged: ");
+            // print the text file back to the user
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Kevin\Desktop\AOL Folder\C# and .NET Framework\Basic_C#_Programs\Input Assignment\log.txt"));
+            Console.ReadLine();
+        }
+    }
+}
