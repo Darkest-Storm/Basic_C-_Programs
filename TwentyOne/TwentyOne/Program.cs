1a0c49d4da116f78952b53300d38c23fa00d3ecb
Darkest-Storm <KevinSurmik@gmail.com>
2024-11-28 07:39:07 -0800

    moved classes to a library

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+using Casino;
+using Casino.TwentyOne;
 
 namespace TwentyOne
 {
@@ -11,11 +9,6 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            DateTime yearOfBirth = new DateTime(1992, 04, 14, 9, 23, 45);
-            DateTime yearOfGrad = new DateTime(2010, 6, 12, 14, 30, 45);
-
-            TimeSpan ageAtGrad = yearOfGrad - yearOfBirth;
-
             Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name");
             string playerName = Console.ReadLine();
             Console.WriteLine("And how much money did you bring today?");
@@ -25,6 +18,12 @@ namespace TwentyOne
             if (answer == "yes" || answer =="yeah" || answer =="y" || answer =="ya")
             {
                 Player player = new Player(playerName, bank);
+                player.Id = Guid.NewGuid();
+                using (StreamWriter file = new StreamWriter(@"C:\Users\Kevin\Desktop\AOL Folder\C# and .NET Framework non repo\Logs\TwentyOne Logs\log.txt", true))
+                {
+                    file.WriteLine(DateTime.Now);
+                    file.WriteLine(player.Id);
+                }
                 Game game = new TwentyOneGame();
                 game += player;
                 player.isActivelyPlaying = true;
