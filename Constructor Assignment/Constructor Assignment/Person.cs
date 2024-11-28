a6bf70839dd862921df3e86ea1e153fa492b7bf0
Darkest-Storm <KevinSurmik@gmail.com>
2024-11-28 09:56:33 -0800

    Constructor Assignment

---
+++ Constructor Assignment/Constructor Assignment/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Constructor_Assignment
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Occupation { get; set; }
+
+        // Chain two constructors together
+        // Base constructor with all parameters
+        public Person(string name, string occupation)
+        {
+            Name = name;
+            Occupation = occupation;
+            Console.WriteLine("Created person {0} with Occupation: {1}", name, occupation);
+        }
+
+        // Constructor chaining with default occupation
+        public Person(string name) : this(name, "Default-Unemployed")
+        {
+        }
+    }
+}
+
---
+++ Constructor Assignment/Constructor Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Constructor_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a const variable
+            const string Welcome = "Welcome to the Constructor Assignment";
+            Console.WriteLine(Welcome);
+
+            // create a variable using var
+            Console.WriteLine("Please enter your name:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Please enter your occupation or press enter:");
+            var occ = Console.ReadLine();
+
+            // calling constructor based on if user entered an occupation or just use default
+            Person person = string.IsNullOrWhiteSpace(occ) ? new Person(name) : new Person(name, occ);
+
+            Console.ReadLine();
+        }
+    }
+}
